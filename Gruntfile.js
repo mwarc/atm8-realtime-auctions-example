e08bcf9be84ea1fb30592dc85b4e868ad31ca44a
Tomasz Rybak <tomasz.rybak@allegro.pl>
2015-09-12 20:37:11 +0200

    offer page layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
         copy: {
@@ -26,6 +27,12 @@ module.exports = function(grunt) {
                 files: {
                     "public/styles/main.css": "src/main/resources/styles/index.less"
                 }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['src/main/resources/styles/**/*.less'],
+                tasks: ['less:main']
             }
         }
     });
