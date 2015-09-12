4301e3c8bd528c4963a6daff5cd797dfbcc12f7f
Tomasz Rybak <tomasz.rybak@allegro.pl>
2015-09-12 15:54:52 +0200

    less setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,34 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.initConfig({
         copy: {
             libs: {
                 src: 'bower_components/sockjs-client/dist/sockjs-0.3.4.min.js',
-                dest: 'public/js/sockjs.min.js'
+                dest: 'public/scripts/sockjs.min.js'
+            },
+            scripts: {
+                src: 'src/main/resources/scripts/realtime-auctions.js',
+                dest: 'public/scripts/realtime-auctions.js'
             }
         },
         uglify: {
             libs: {
                 src: 'bower_components/vertx3-eventbus-client/vertxbus.js',
-                dest: 'public/js/vertxbus.min.js'
+                dest: 'public/scripts/vertxbus.min.js'
+            }
+        },
+        less: {
+            main: {
+                files: {
+                    "public/styles/main.css": "src/main/resources/styles/index.less"
+                }
             }
         }
     });
 
-    grunt.registerTask('build', ['copy:libs', 'uglify:libs']);
+    grunt.registerTask('build', ['copy', 'uglify:libs', 'less:main']);
 
 };
