bbca3c7f14e5d247f6347aedbd634d80979569f8
Tomasz Rybak <tomasz.rybak@allegro.pl>
2015-09-14 18:05:55 +0200

    init commit
    (cherry picked from commit 4c1cf3b7997da9c4c6d4d2d8e36ea35cdb6bb579)

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,30 +6,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
-        copy: {
-            sockjs: {
-                src: 'bower_components/sockjs-client/dist/sockjs-0.3.4.min.js',
-                dest: 'public/scripts/sockjs.min.js'
-            },
-            angular: {
-                src: 'bower_components/angular/angular.min.js',
-                dest: 'public/scripts/angular.min.js'
-            },
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/main/resources/scripts/',
-                    src: '**/*.{html,js}',
-                    dest: 'public/scripts/'
-                }]
-            }
-        },
-        uglify: {
-            libs: {
-                src: 'bower_components/vertx3-eventbus-client/vertxbus.js',
-                dest: 'public/scripts/vertxbus.min.js'
-            }
-        },
         less: {
             main: {
                 files: {
@@ -41,14 +17,10 @@ module.exports = function(grunt) {
             styles: {
                 files: ['src/main/resources/styles/**/*.less'],
                 tasks: ['less:main']
-            },
-            scripts: {
-                files: ['src/main/resources/scripts/**/*.{html,js}'],
-                tasks: ['copy:main']
             }
         }
     });
 
-    grunt.registerTask('build', ['copy', 'uglify:libs', 'less:main']);
+    grunt.registerTask('build', ['less:main']);
 
 };
